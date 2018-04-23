8e8e42e0aeb2122e861564ab3e310746396f303b
ABADIE Loïc <loic.abadie@pictime.com>
2018-04-23 16:09:56 +0200

    css

---
+++ static/doc.js
@@ -0,0 +1,40 @@
+
+Vue.component('elk', {
+  props: ['title'],
+  template: `
+    <div>
+      test_ELK
+    </div>
+  `,computed: {
+  },
+  data: function () {
+      return {
+          suffix_url: "",
+      };
+  },
+  methods: {
+    call_curl_cmd(cmd){
+      _this         = this
+      axios.post('test/curl_cmd', {
+        command: ""
+      })
+      .then(function (response) {
+        console.log("response", response, "data: ", response.data);
+      })
+      .catch(function (error) {
+        _this.m_json_return = error
+        console.log("error:", error);
+      });
+    }
+  },
+})
+
+console.log("*****defined ");
+var app = new Vue({
+  el: '#app',
+  delimiters: ['${', '}'],
+  data:{
+  },
+  watch: {
+    }
+});
