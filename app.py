ef5423098b271a14a59470a3ecef749530a24d82
ABADIE Loïc <loic.abadie@pictime.com>
2018-04-24 13:59:40 +0200

    colors

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import render_template
 from elasticsearch import Elasticsearch
+import json
 
 
 es  = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     # basi request test elasticpath
     res = es.search(index="kiabi", body={"size":0, "aggs" : { "result" : { "terms" : { "field" : "color.keyword", "size": 10}}}})
     print(res["aggregations"]["result"]["buckets"])
+
     return render_template('test_documentation.html', color_agreggation=res["aggregations"]["result"]["buckets"])
+
+@app.route('/match', methods=['POST'])
+def match():
+    # simple matching test
+    args    = request.json.get("value")
+    res     = es.search(index="kiabi", body={ "size":50, "query": { "match" : args}})
+    return jsonify(res["hits"]["hits"])
--- static/doc.js
+++ static/doc.js
@@ -1,43 +1,57 @@
 
-Vue.component('elk', {
-  props: ['title'],
+Vue.component('productlist', {
   template: `
     <div>
-      test_ELK
+      <div v-for="(product, index)  in products" v-bind:class="{one_line : true, product_hilighed : true, tooltipped : true}" v-on:mouseover="mouseOver">
+        <img  class="product_result " :src="product._source.image_link_https"  :alt="index">
+      </div>
     </div>
   `,computed: {
   },
   data: function () {
       return {
-          suffix_url: "",
+          products: []
       };
   },
   methods: {
-    call_curl_cmd(cmd){
-      _this         = this
-      axios.post('test/curl_cmd', {
-        command: ""
-      })
-      .then(function (response) {
-        console.log("response", response, "data: ", response.data);
-      })
-      .catch(function (error) {
-        _this.m_json_return = error
-        console.log("error:", error);
-      });
-    }
+     mouseOver: function(e){
+       var idx = e.target.alt
+
+        console.log(this.products[idx]._source);
+     }
   },
 })
 
 var app = new Vue({
   el: '#app',
   delimiters: ['${', '}'],
+  methods: {
+    change: function(e, value) {
+      //receive the value selected (return an array if is multiple)
+      console.log("changedValue", e.target.value, this.$refs.productlist)
+      //console.log(this.$refs.value);
+      var arg   = { "color" : e.target.value }
+      var _this = this
+
+      axios.post('match', {
+        value: arg
+      })
+      .then(function (response) {
+        console.log("response", response, "data: ", response.data);
+        console.log("---> ", _this.$refs.productlist.products );
+        _this.$refs.productlist.products = response.data
+      })
+      .catch(function (error) {
+        console.log("error:", error);
+      });
+    }
+  },
   data:{
   },
   watch: {
     }
 });
 
-var elem = document.querySelector('select');
- var instance = M.FormSelect.init(elem);
+var elem      = document.querySelector('select');
+var instance  = M.FormSelect.init(elem);
 console.log("done");
